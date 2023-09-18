336358865b41ecd189ff9df4558efa3e81c3871d
yardenyo <gmloler@gmail.com>
2023-09-18 12:48:31 +0300

    implement footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,34 @@
 <template>
-	<div class="navbar">1234</div>
+	<div class="footer">
+		<div class="copyright">
+			<div class="footer-text">© {{ getCurrentYear() }} Yarden Yosef. All rights reserved.</div>
+			<div class="footer-text">Designed & Developed by Yarden Yosef.</div>
+		</div>
+	</div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 
 export default defineComponent({
-	name: "Navbar",
+	name: "Footer",
 	setup() {
-		return {};
+		function getCurrentYear() {
+			return new Date().getFullYear();
+		}
+		return {
+			getCurrentYear,
+		};
 	},
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.footer {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	padding: 1rem;
+}
+</style>
